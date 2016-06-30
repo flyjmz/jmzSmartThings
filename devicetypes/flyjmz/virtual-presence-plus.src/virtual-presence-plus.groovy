4d3215184aba89ac271aa1c5ff281a0764a3c2ea
Jesse Ziegler <ziegler.jesse@gmail.com>
2016-06-30 16:53:26 -0500

    Initial Upload

---
+++ devicetypes/flyjmz/virtual-presence-plus.src/virtual-presence-plus.groovy
@@ -0,0 +1,75 @@
+/**
+* Copyright 2016 Combined effort: flyjmz and Austin Pritchett and SmartThings
+*
+* Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+* in compliance with the License. You may obtain a copy of the License at:
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+* for the specific language governing permissions and limitations under the License.
+*
+*/
+//Release History
+//		1.0 May 20, 2016
+//			Initial Release
+//      9June2016 flyjmz modified lines below to prevent both switch and presence logging, previous code is //commented-out// 
+
+
+metadata {
+        definition (name: "Virtual Presence Plus", namespace: "flyjmz", author: "flyjmz") {
+        capability "Switch"
+        capability "Refresh"
+        capability "Presence Sensor"
+		capability "Sensor"
+        
+		command "arrived"
+		command "departed"
+    }
+
+	// simulator metadata
+	simulator {
+	}
+
+	// UI tile definitions
+	tiles {
+		standardTile("button", "device.switch", width: 2, height: 2, canChangeIcon: false,  canChangeBackground: true) {
+			state "off", label: 'Away', action: "switch.on", icon: "st.Kids.kid10", backgroundColor: "#ffffff"     //  , nextState: "on"
+			state "on", label: 'Present', action: "switch.off", icon: "st.Kids.kid10", backgroundColor: "#53a7c0"  //  , nextState: "off"
+		}
+		standardTile("refresh", "device.switch", inactiveLabel: false, decoration: "flat") {
+			state "default", label:'', action:"refresh.refresh", icon:"st.secondary.refresh"
+		}
+        standardTile("presence", "device.presence", width: 1, height: 1, canChangeBackground: true) {
+			state("present", labelIcon:"st.presence.tile.mobile-present", backgroundColor:"#53a7c0")
+			state("not present", labelIcon:"st.presence.tile.mobile-not-present", backgroundColor:"#ffffff")
+		}
+		main (["button", "presence"])
+		details(["button", "presence", "refresh"])
+	}
+}
+
+def parse(String description) {
+	def pair = description.split(":")
+	createEvent(name: pair[0].trim(), value: pair[1].trim())
+}
+
+// handle commands
+def arrived() {
+	on()
+}
+
+def departed() {
+    off()
+}
+
+def on() {
+	sendEvent(name: "switch", value: "on")
+    sendEvent(name: "presence", value: "present")
+}
+
+def off() {
+	sendEvent(name: "switch", value: "off")
+    sendEvent(name: "presence", value: "not present")
+}
