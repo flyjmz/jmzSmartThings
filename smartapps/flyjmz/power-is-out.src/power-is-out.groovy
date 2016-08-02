2f3feaf1de1d88179fca14a7969be325d758c5ec
Jesse Ziegler <ziegler.jesse@gmail.com>
2016-08-02 10:44:33 -0500

    Updated comments

--- smartapps/flyjmz/power-is-out.src/power-is-out.groovy
+++ smartapps/flyjmz/power-is-out.src/power-is-out.groovy
@@ -12,12 +12,14 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
+ *	Version 1.1    2 August 2016 - updated notes within app to clarify.
+ *	To do: give options when power is back on to turn off/on lights, switches, etc (like cree bulbs, etc)
  */
 definition(
     name: "Power Is Out",
     namespace: "flyjmz",
     author: "flyjmz",
-    description: "Alert me of power loss using motion detector's change from wired-power to battery-power. SmartThings hub and internet connection must be working! You can connect the hub and internet connection device (e.g. modem, router, etc.) to a battery backup power strip so that the motion detector and detect the loss and the hub and router will still have enough power to get the message out before they fail as well.",
+    description: "Alert me of power loss using SmartSense Motion v1's change from wired-power to battery-power.  Note: SmartThings hub and internet connection must be working! You can connect the hub and internet connection device (e.g. modem, router, etc.) to a UPS (uniteruptable power supply) so they stay powered even when the house looses power.  Then the motion detector can detect the loss and the hub and router will still have enough power to get the message out before they fail as well.",
     category: "Safety & Security",
     iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
     iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png"
@@ -27,16 +29,17 @@ definition(
 preferences {
 	section("When there is wired-power loss on...") {
 			input "motion1", "capability.motionSensor", title: "Where?"
+            paragraph "Must be a SmartSense Motion v1.  Also, SmartThings Hub and internet connection (modem/router) must retain power for this to work (e.g. connect them to a UPS)."
 	}
-	section("Via a push notification and a text message(optional)"){
-    	input "pushAndPhone", "enum", title: "Send Text?", required: false, metadata: [values: ["Yes","No"]]
-		input "phone1", "phone", title: "Phone Number (for Text, optional)", required: false
-
+	section("Enter Phone number if you want a text message (optional) as well as a push notificaiton."){
+    	input "pushAndPhone", "enum", title: "Send SMS?", required: false, metadata: [values: ["Yes","No"]]
+		input "phone1", "phone", title: "Phone Number (only for SMS, optional)", required: false
 	}
 }
 
 def installed() {
-	subscribe(motion1, "powerSource.battery", onBatteryPowerHandler)
+	unsubscribe()
+    subscribe(motion1, "powerSource.battery", onBatteryPowerHandler)
     subscribe(motion1, "powerSource.powered", PoweredPowerHandler)
 }
 
@@ -45,7 +48,6 @@ def updated() {
 	subscribe(motion1, "powerSource.battery", onBatteryPowerHandler)
     subscribe(motion1, "powerSource.powered", PoweredPowerHandler)
 }
-
 
 def onBatteryPowerHandler(evt) {
 	log.trace "$evt.value: $evt"
